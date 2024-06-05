220c66ac10a689274d65e7f4e29665f88737f86f
Luke Leber <lal65@psu.edu>
2024-06-05 13:09:21 -0400

    Fix

--- src/Functions/Styles.php
+++ src/Functions/Styles.php
@@ -21,7 +21,7 @@ class Styles {
       'node_modules' . DIRECTORY_SEPARATOR . '@psu-ooe',
       'vendor' . DIRECTORY_SEPARATOR . 'psu-ooe' . DIRECTORY_SEPARATOR . 'static-site-generator' . DIRECTORY_SEPARATOR . 'node_modules' . DIRECTORY_SEPARATOR . '@psu-ooe',
     ], static fn($path) => file_exists($path));
-    foreach (glob($path[0] . '/*/package.json') as $manifest) {
+    foreach (glob(current($path) . '/*/package.json') as $manifest) {
       $manifest_json = json_decode(file_get_contents($manifest), TRUE, 512, JSON_THROW_ON_ERROR);
       $manifests[$manifest_json['name']] = $manifest_json;
     }
