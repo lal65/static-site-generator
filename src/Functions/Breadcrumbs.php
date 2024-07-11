4fa9175f8b966f292e18e11f1ed5278a1666a7f0
Luke Leber <lal65@psu.edu>
2024-07-11 11:01:28 -0400

    ..

--- src/Functions/Breadcrumbs.php
+++ src/Functions/Breadcrumbs.php
@@ -8,7 +8,7 @@ use League\CommonMark\Extension\FrontMatter\FrontMatterParser;
 class Breadcrumbs {
   public static function getBreadcrumbs(string $path): array {
     $base_path = Config::getConfig('base_path');
-    $crumbs = [['url' => $base_path, 'text' => 'Home']];
+    $crumbs = [['url' => '/' . $base_path, 'text' => 'Home']];
 
     $frontMatterParser = new FrontMatterParser(new LibYamlFrontMatterParser());
     $root = '';
@@ -17,7 +17,7 @@ class Breadcrumbs {
       $metadata = $frontMatterParser->parse(file_get_contents('pages/' . $root . $part_without_extension . '.md'))->getFrontMatter();
 
       if (!str_ends_with($part, '.md')) {
-        $crumbs[] = ['url' => $base_path . $root . '/' . $part_without_extension, 'text' => $metadata['page_title']];
+        $crumbs[] = ['url' => '/' . ltrim($base_path . '/' . $root . $part_without_extension, '/'), 'text' => $metadata['page_title']];
       }
       else {
         $crumbs[] = ['text' => $metadata['page_title']];
